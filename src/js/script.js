ef9eca2753664e62a1ba0c1ef4cf05943a65018d
fevunge <developervunge@gmail.com>
2025-04-23 03:28:15 +0000

    feat[index] adicionar um relogio

--- src/js/script.js
+++ src/js/script.js
@@ -23,7 +23,19 @@ keys.forEach(key => {
 keys.forEach(key => {
     key.addEventListener('transitionend', (e) => {
         if (e.propertyName !== 'transform') return;
-        console.log(e.propertyName);
         key.classList.remove('play');
     });
 });
+
+const ruler = document.querySelector('.time');
+function runRuler(){
+	const timeNow = new Date();
+	const hours = String(timeNow.getHours()).padStart(2, 0)
+	const minutes = String(timeNow.getMinutes()).padStart(2, 0)
+	const seconds = String(timeNow.getSeconds()).padStart(2, 0)
+	ruler.textContent = `${hours}:${minutes}:${seconds}`
+}
+
+runRuler();
+setInterval(runRuler, 1000);
+

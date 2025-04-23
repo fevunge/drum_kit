675242c179c08097d212b894d4bf1c7be45e4998
fevunge <developervunge@gmail.com>
2025-04-23 02:40:42 +0000

    feat[sounds] implementar acao tocar

--- src/js/script.js
+++ src/js/script.js
@@ -2,5 +2,12 @@ window.addEventListener('keydown', (e) => {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     key.classList.add('play');
+
+    if (!audio) return;
+    audio.play();
+    audio.currentTime = 0;
+    audio.addEventListener('ended', () => {
+        key.classList.remove('play');
+    });
 });
 

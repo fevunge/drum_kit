4c1bf6143f6b747201942fd95b9643fc76b4ccd8
fevunge <developervunge@gmail.com>
2025-04-23 02:57:12 +0000

    feat[keyboard] adicionar funcao de toque para mobile

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
+
 window.addEventListener('keydown', (e) => {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
@@ -9,6 +10,16 @@ window.addEventListener('keydown', (e) => {
 });
 
 const keys = document.querySelectorAll('.key');
+keys.forEach(key => {
+    key.addEventListener('click', (e) => {
+        const audio = document.querySelector(`audio[data-key="${key.dataset.key}"]`);
+        if (!audio) return;
+        audio.currentTime = 0;
+        audio.play();
+        key.classList.add('play');
+    });
+}
+
 keys.forEach(key => {
     key.addEventListener('transitionend', (e) => {
         if (e.propertyName !== 'transform') return;

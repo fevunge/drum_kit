7f2ad90ba5d1cf9d8625164b6caf7e52fa7159d0
fevunge <developervunge@gmail.com>
2025-04-23 02:37:52 +0000

    feat[sounds] adicionar os arquivos de som

---
+++ src/js/script.js
@@ -0,0 +1,6 @@
+window.addEventListener('keydown', (e) => {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    key.classList.add('play');
+});
+
